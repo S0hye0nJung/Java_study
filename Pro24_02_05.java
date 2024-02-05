ea86d73f64569863b54e93fab9ea3dde15af5c24
S0hye0nJung <158130588+S0hye0nJung@users.noreply.github.com>
2024-02-05 14:10:19 +0900

    Add files via upload
    2024_02_05
    Day1

---
+++ Pro24_02_05.java
@@ -0,0 +1,57 @@
+package programmers;
+
+import java.util.Scanner;
+
+public class Pro24_02_05 {
+
+	public static void main(String[] args) {
+		///2024.02.05 문제풀이(Day1)- 기초 부분
+		
+		//1. 문자열 출력하기
+		Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println(a);
+        
+        
+        //2. a와 b 출력하기
+//        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//
+//        System.out.println("a = "+ a );
+//        System.out.println("b = "+ b );
+//        
+		
+        //3. 문자열 반복해서 출력하기
+//		Scanner sc = new Scanner(System.in);
+//        String str = sc.next();
+//        int n = sc.nextInt();
+//        for (int i=1; i<=n;i++){
+//            System.out.print(str);
+//        }
+		
+		
+		//4. 대소문자 바꿔서 출력하기
+//        Scanner sc = new Scanner(System.in);
+//        String a = sc.next();
+//        for (int i=0; i<a.length();i++) {
+//        	int temp = (int) a.charAt(i);
+//        	if(temp>=65 &temp<=90) {
+//        		System.out.print((char)(temp +32));
+//        	}else {
+//        		System.out.print((char)(temp -32));
+//        	}
+//        }
+		
+		
+        //5. 특수문자 출력하기 !@#$%^&*(\'"<>?:;
+//        System.out.println("!@#$%^&*(\\'\"<>?:;");
+		
+		
+		
+
+
+	}
+
+
+	}
