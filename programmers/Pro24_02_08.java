45bc4c88dfce8005b68242710bd138c258806ddd
S0hye0nJung <158130588+S0hye0nJung@users.noreply.github.com>
2024-02-08 09:31:45 +0900

    day_4

---
+++ programmers/Pro24_02_08.java
@@ -0,0 +1,121 @@
+package programmers;
+
+import java.util.Scanner;
+
+public class Pro24_02_08 {
+
+	public static void main(String[] args) {
+		///2024.02.08 문제풀이(Day4)- 기초 부분
+		
+//		//1. n의 배수
+//		class Solution {
+//		    public int solution(int num, int n) {
+//		        int answer;
+//		        if(num%n==0){
+//		            answer=1;
+//		        }else{
+//		            answer=0;
+//		        }
+//		        return answer;
+//		    }
+//		}
+//		
+		
+		
+		
+//		//2. 공배수
+//		class Solution {
+//		    public int solution(int number, int n, int m) {
+//		        int answer;
+//		        if(number%n==0 && number%m==0){
+//		            answer =1;
+//		        }else{
+//		            answer =0;
+//		        }
+//		        return answer;
+//		    }
+//		}
+//		
+//		
+		
+//		//3. 홀짝에 따라 다른 값 반환하기
+//		class Solution {
+//		    public int solution(int n) {
+//		        int answer=0;
+//		        if(n%2==0){
+//		            for(int i=0; i<=n; i+=2){
+//		                answer+=Math.pow(i,2);
+//		            }
+//		        }else{
+//		            for(int i =1; i<=n; i+=2){
+//		                answer+=i;
+//		        }
+//		    }
+//		        return answer;
+//		}}
+//		
+//		
+		
+//		//4. 조건 문자열
+//		class Solution {
+//		    public int solution(String ineq, String eq, int n, int m) {
+//		        int result=0;
+//		        switch(ineq+eq){
+//		            case ">=" : if(n >= m){
+//		                result= 1;
+//		                break;
+//		            }else{
+//		                result=0;
+//		                break;
+//		            }
+//		            case "<=" : if(n <= m){
+//		                result= 1;
+//		                break;
+//		            }else{
+//		                result= 0;
+//		                break;
+//		            }
+//		            case ">!" : if(n > m){
+//		                result= 1;
+//		                break;
+//		            }else{
+//		                result= 0;
+//		                break;
+//		            }
+//		                case "<!" : if(n < m){
+//		                result= 1;
+//		                    break;
+//		            }else{
+//		                result= 0;
+//		                    break;
+//		            }
+//		        }
+//		        return result; 
+//		        }
+//		    }
+		
+		
+		
+		//5.  flag에 따라 다른 값 반환하기
+//		
+//		class Solution {
+//		    public int solution(int a, int b, boolean flag) {
+//		        int answer = 0;
+//		        if(flag) {
+//		            answer=a+b;
+//		        }else{
+//		            answer =a-b;
+//		        }
+//		        return answer;
+//		    }
+//		}
+//		
+		
+		
+		
+		
+
+	}//void메소드 end
+
+
+	}//class end
