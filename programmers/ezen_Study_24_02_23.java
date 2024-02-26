a47f140182320843060cf8511fa21d0ff1cd4074
S0hye0nJung <158130588+S0hye0nJung@users.noreply.github.com>
2024-02-26 19:24:20 +0900

    24.02.23(혜린님 문제-풀이)

---
+++ programmers/ezen_Study_24_02_23.java
@@ -0,0 +1,57 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ezen_Study_24_02_23 {
+
+	public static void main(String[] args) {
+		/*1. 출력문처럼 words 배열에서 단어의 길이가 5이상인 단어들만 보여주도록 해주세요!!
+		2. 메서드 완성 후 에러가 발생하는 요소도 찾아서 해결해 주세요!! 
+		(에러는 메서드 return 과 반복문을 보시면 쉽게 발견할 수 있습니다.)
+
+		=======================================
+		<출력문>
+		5글자 이상인 단어들 :
+		banana
+		orange
+		strawberry
+		========================================
+
+		*/
+		    
+		String[] words = {"leaf", "banana", "orange", "strawberry", "pear", "cat"};
+		List<String> wordsList = new ArrayList<>();
+
+		        for (int i = 0; i < words.length; i++) {
+		            wordsList.add(words[i]);
+		        }
+
+		  //메서드 호출 및 출력문
+		        List<String> filteredWords = ezen_Study_24_02_23.getFilteredWords(wordsList);
+				
+			 System.out.println("5글자 이상인 단어들의 리스트:");
+
+			//메서드로 필터링한 글자들을 for문을 통해 프린트
+			 for (String word : filteredWords) {
+			       System.out.println(word);
+			 }
+		    }//main메소드 끝
+
+	public static List<String> getFilteredWords(List<String> wordsList) {
+		        
+			List<String> filteredWords = new ArrayList<>();
+		        //1. 여기에 5글자 이상인 단어들만 filteredWords 리스트에 추가하도록 구현()
+		        
+		        for(int i=0;i<wordsList.size();i++) {
+		        	if(wordsList.get(i).length() >5) {
+		        		filteredWords.add(wordsList.get(i));
+		        	}
+		        	}
+		     
+		        return filteredWords;
+		    }
+		
+	}
+
+
