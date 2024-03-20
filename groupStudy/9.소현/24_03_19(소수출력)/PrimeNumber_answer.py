e694b57a0fbcd0394aa6a06bffbf9db9bc2cffde
S0hye0nJung <158130588+S0hye0nJung@users.noreply.github.com>
2024-03-20 16:06:46 +0900

    Update PrimeNumber_answer.py

--- groupStudy/9.소현/24_03_19(소수출력)/PrimeNumber_answer.py
+++ groupStudy/9.소현/24_03_19(소수출력)/PrimeNumber_answer.py
@@ -1,3 +1,15 @@
-# 내일(24/03/20(수))에 올릴 예정
+# 문제에 대한 답
+num =int(input("숫자를 입력하세요 >> "))
+for i in range(2, num+1):
+    # if(i==2):
+    #     print(i)
+    for j in range(2, i+1):
+        if(i == j):
+            print(i)  
+        elif(i%j == 0):
+            break;
+        
+    
+
 
 
